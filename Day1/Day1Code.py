64043517fcb92b723b74eb7a6215aaba152160e3
kanealdrich121 <kanealdrich12@gmail.com>
2022-12-01 13:14:06 -0500

    Advent Day 1 Solution

---
+++ Day1/Day1Code.py
@@ -0,0 +1,15 @@
+calInput = open('./input.txt', 'r')
+allLines = calInput.readlines()
+thisElfTotal = 0
+bestElfTotal = 0
+allTotals = []
+for line in allLines:
+    if line == '\n':
+        if thisElfTotal > bestElfTotal:
+            allTotals.append(int(thisElfTotal))
+        thisElfTotal = 0
+    else:
+        thisElfTotal += int(line)
+allTotals.sort(reverse=True)
+top3 = allTotals[0] + allTotals[1] + allTotals[2]
+print('Best Elf: ', allTotals[0], 'Top 3: ', top3)
