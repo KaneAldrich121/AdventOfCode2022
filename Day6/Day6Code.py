a255936b966cd6fbda24026900e7ee9b1b4a036b
kanealdrich121 <kanealdrich12@gmail.com>
2022-12-06 10:44:17 -0500

    Adding Day 6 Files

---
+++ Day6/Day6Code.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+def findStartMarker(input, lengthOfMarker):
+    for index in range(0, len(input)):
+        thisWindow = input[index:index+lengthOfMarker]
+        if len(Counter(thisWindow)) == lengthOfMarker:
+            return index + lengthOfMarker
+    raise "Could not find solution"
+
+
+signalInput = open('./input.txt', 'r')
+signalLines = signalInput.readlines()[0]
+print("Part 1: ", findStartMarker(signalLines, 4))
+print("Part 2: ", findStartMarker(signalLines, 14))
+
